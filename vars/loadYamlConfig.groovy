00c80465f43d40e8b8873b6c1fe5a332fabd6f42
Justin Ho <justinho@hosvr.com>
2019-09-07 02:06:05 -0400

    remove debug lines

--- vars/loadYamlConfig.groovy
+++ vars/loadYamlConfig.groovy
@@ -8,12 +8,9 @@ def call(script, yamlFile){
   checkout scm
 
   def pipelineDefinition = readYaml(file: yamlFile)
-  print(pipelineDefinition.dump())
 
   build = new YamlBuild(this, pipelineDefinition)
-  print(build.dump())
 
   build.start()
 
 }
-
